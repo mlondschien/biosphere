962319f290bb172a45b0147a6e25bbebf228b372
Malte Londschien <malte@londschien.de>
2021-12-23 11:20:57 +0100

    Use random data.

--- benches/bench_forest.rs
+++ benches/bench_forest.rs
@@ -1,26 +1,31 @@
-#[allow(non_snake_case)]
 use criterion::{criterion_group, criterion_main, Criterion};
 use oobforest::RandomForest;
 
-use csv::ReaderBuilder;
-use ndarray::{s, Array2};
-use ndarray_csv::Array2Reader;
-use std::fs::File;
+#[cfg(test)]
+use ndarray::{Array, Array1, Array2};
+use ndarray_rand::rand_distr::Uniform;
+use ndarray_rand::RandomExt;
+use rand::rngs::StdRng;
+use rand::SeedableRng;
 
-pub fn iris() -> Array2<f64> {
-    let file = File::open("testdata/iris.csv").unwrap();
-    let mut reader = ReaderBuilder::new().has_headers(true).from_reader(file);
-    let data: Array2<f64> = reader.deserialize_array2((150, 5)).unwrap();
-    data
+pub fn data(seed: u64) -> (Array2<f64>, Array1<f64>) {
+    let mut rng = StdRng::seed_from_u64(seed);
+    let n = 10000;
+    let X = Array::random_using((n, 100), Uniform::new(0., 1.), &mut rng);
+    let y = Array::random_using(n, Uniform::new(0., 1.), &mut rng);
+    let y = y + X.column(0) + X.column(1).map(|x| x - x * x);
+
+    (X, y)
 }
 
+#[allow(non_snake_case)]
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let data = iris();
-    let X = data.slice(s![.., 0..4]);
-    let y = data.slice(s![.., 4]);
+    let seed = 0;
+    let (X, y) = data(seed);
 
-    let forest = RandomForest::new(&X, &y, None, Some(8), None, None, None, None);
-
+    let X_view = X.view();
+    let y_view = y.view();
+    let forest = RandomForest::new(&X_view, &y_view, None, Some(8), None, None, None, None);
     c.bench_function("forest", |b| b.iter(|| forest.predict()));
 }
 
