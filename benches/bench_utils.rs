9503c51e1466f1acb485c1934b1f38cedf9ae5d8
Malte Londschien <malte@londschien.de>
2022-01-04 16:40:15 +0100

    Benchmark all utility functions.

--- benches/bench_utils.rs
+++ benches/bench_utils.rs
@@ -1,4 +1,6 @@
-use biosphere::utils::argsort;
+use biosphere::utils::{
+    argsort, oob_samples_from_weights, sample_indices_from_weights, sample_weights,
+};
 #[cfg(test)]
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use ndarray::Array;
@@ -7,7 +9,7 @@ use ndarray_rand::RandomExt;
 use rand::rngs::StdRng;
 use rand::SeedableRng;
 
-pub fn benchmark_argsort(c: &mut Criterion) {
+pub fn benchmark_utils(c: &mut Criterion) {
     let seed = 0;
     let mut group = c.benchmark_group("argsort");
     let sizes: &[usize] = &[1000, 10000, 100000, 1000000];
@@ -22,8 +24,19 @@ pub fn benchmark_argsort(c: &mut Criterion) {
         group.bench_with_input(BenchmarkId::new("argsort_one_hot", size), &y, |b, x| {
             b.iter(|| argsort(&x))
         });
+        group.bench_function(format!("sample_weight, size={}", size), |b| {
+            b.iter(|| sample_weights(size, &mut rng))
+        });
+        let weights = sample_weights(size, &mut rng);
+        let indices = vec![argsort(&x)];
+        group.bench_function(format!("sample_indices_from_weights, size={}", size), |b| {
+            b.iter(|| sample_indices_from_weights(&weights, &indices))
+        });
+        group.bench_function(format!("oob_samples_from_weights, size={}", size), |b| {
+            b.iter(|| oob_samples_from_weights(&weights))
+        });
     }
 }
 
-criterion_group!(bench_argsort, benchmark_argsort);
-criterion_main!(bench_argsort);
+criterion_group!(bench_utils, benchmark_utils);
+criterion_main!(bench_utils);
