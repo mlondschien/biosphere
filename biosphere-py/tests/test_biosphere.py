78e7cb31fc4532ba999e816cc1defd16abb1aa21
Malte Londschien <malte@londschien.de>
2022-01-07 14:09:28 +0100

    Fix tests.

--- biosphere-py/tests/test_biosphere.py
+++ biosphere-py/tests/test_biosphere.py
@@ -1,15 +1,38 @@
 import numpy as np
 from pathlib import Path
-from biosphere import predict_oob
+from biosphere import RandomForest, DecisionTree
 
 _IRIS_FILE = "iris.csv"
 _IRIS_PATH = Path(__file__).resolve().parents[2] / "testdata" / _IRIS_FILE
 
-def test_predict_oob():
+
+def test_forest():
     data = np.loadtxt(_IRIS_PATH, skiprows=1, delimiter=",", usecols=(0, 1, 2, 3, 4))
     X = data[:, 0:4]
     y = data[:, 4]
-    result = predict_oob(X, y, 100, None, None, 2, 1, 0)
-    mse = np.mean((result - y) ** 2)
 
-    assert mse < 0.05
+    random_forest = RandomForest()
+    oob_predictions = random_forest.fit_predict_oob(X, y)
+    predictions = random_forest.predict(X)
+
+    oob_mse = np.mean((oob_predictions - y) ** 2)
+    mse = np.mean((predictions - y)**2)
+    
+    assert oob_mse < 0.05
+    assert mse < oob_mse / 2
+
+
+def test_tree():
+    data = np.loadtxt(_IRIS_PATH, skiprows=1, delimiter=",", usecols=(0, 1, 2, 3, 4))
+    X = data[:, 0:4]
+    y = data[:, 4]
+
+    random_forest = RandomForest()
+    oob_predictions = random_forest.fit_predict_oob(X, y)
+    predictions = random_forest.predict(X)
+
+    oob_mse = np.mean((oob_predictions - y) ** 2)
+    mse = np.mean((predictions - y)**2)
+    
+    assert oob_mse < 0.05
+    assert mse < oob_mse / 2
