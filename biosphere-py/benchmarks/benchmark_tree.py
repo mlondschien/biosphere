ce111e5c90ca8466b17d095396dff88e281c2c2b
Malte Londschien <61679398+mlondschien@users.noreply.github.com>
2022-02-19 17:16:53 +0100

    Make asv benchmarks compatible with biosphere 0.1.0.. (#58)

--- biosphere-py/benchmarks/benchmark_tree.py
+++ biosphere-py/benchmarks/benchmark_tree.py
@@ -1,28 +1,25 @@
-from .common import Benchmark
-from biosphere import DecisionTree
 from sklearn.tree import DecisionTreeRegressor
+
+from biosphere import DecisionTree
+
+from .common import Benchmark
+
 
 class BiosphereTree(Benchmark):
     name = "biosphere tree"
     param_names = ["n", "mtry"]
-    params=([1000, 10000, 100000, 1000000], [4, 12])
+    params = ([1000, 10000, 100000, 1000000], [4, 12])
 
     def _setup_model(self, params):
         _, mtry = params
-        self.model = DecisionTree(
-            max_depth=8,
-            mtry=mtry,
-        )
+        self.model = DecisionTree(max_depth=8, mtry=mtry)
+
 
 class ScikitLearnTree(Benchmark):
     name = "scikit-learn tree"
     param_names = ["n", "mtry"]
-    params=([1000, 10000, 100000, 1000000], [4, 12])
+    params = ([1000, 10000, 100000, 1000000], [4, 12])
 
     def _setup_model(self, params):
         _, mtry = params
-        self.model = DecisionTreeRegressor(
-            max_depth=8,
-            max_features=mtry,
-        )
-
+        self.model = DecisionTreeRegressor(max_depth=8, max_features=mtry,)
